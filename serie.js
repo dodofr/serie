ef97f37eaa38a2e2bb4e0418f99026d367b0d22c
Julie et Dorian Chopin <chopin.dorian@gmail.com>
2022-09-01 09:58:46 +0400

    ajout bouton retour

--- serie.js
+++ serie.js
@@ -228,6 +228,11 @@ seriePopulaire.addEventListener("click", () => {
 
     })
 })
+//retour en arriere
+//tableau stockant la recherche
+
+let tabInputrecherche =[]
+
 //Serie que l'on recherche
 
 // partie recherche
@@ -244,7 +249,142 @@ inputRecherche.addEventListener("keyup", () => {
 
 // fonction fetch avec la creation
 function fetchEtCreationContenu() {
-  const input = inputRecherche.value;
+  let input = inputRecherche.value;
+  //recuperation de la recherche pour retour en arriere
+  tabInputrecherche.push(input)
+ 
+  let rechercheEnCour = tabInputrecherche.pop()
+  console.log(rechercheEnCour)
+  let retour = document.getElementById("retour")
+  retour.addEventListener("click", () =>{
+    grid.innerHTML = "";
+    //cc du fetch 
+    fetch(
+      `https://api.themoviedb.org/3/search/tv?api_key=3341d636ea5e718cbe535387f5416379&language=fr&page=1&query=${rechercheEnCour}&include_adult=false`
+    )
+      .then((reponse) => reponse.json())
+      .then((data) => {
+        for (let i = 0; i < 10; i++) {
+          // les creations
+          let div = document.createElement("div");
+          let nomSerie = document.createElement("h2");
+          let DateDeSortie = document.createElement("p");
+          let imageSerie = document.createElement("img");
+          let resumeSerie = document.createElement("p");
+          let voteDeLaserie = document.createElement("p");
+  
+          let btnDelaSerie = document.createElement("button");
+          btnDelaSerie.setAttribute("id", `${data.results[i].id}`);
+          btnDelaSerie.setAttribute("class", "btnClass");
+          // l'affichage
+          nomSerie.innerHTML = data.results[i].name;
+          DateDeSortie.innerHTML = data.results[i].first_air_date;
+          imageSerie.src =
+            "https://image.tmdb.org/t/p/original" + data.results[i].poster_path;
+          resumeSerie.innerHTML = data.results[i].overview;
+          voteDeLaserie.innerHTML = data.results[i].vote_average;
+  
+          btnDelaSerie.innerHTML = "En savoir plus";
+  
+          // placement des elements
+          div.appendChild(nomSerie);
+          div.appendChild(DateDeSortie);
+          div.appendChild(imageSerie);
+          div.appendChild(resumeSerie);
+          div.appendChild(voteDeLaserie);
+  
+          div.appendChild(btnDelaSerie);
+          grid.appendChild(div);
+  
+          // Partie click pour en savoir plus
+  
+          let allBtn = document.getElementById(`${data.results[i].id}`);
+          allBtn.addEventListener("click", (e) => {
+            grid.innerHTML = "";
+            fetch(
+              `https://api.themoviedb.org/3/tv/${e.target.id}?api_key=3341d636ea5e718cbe535387f5416379&language=fr`
+            )
+              .then((reponse) => reponse.json())
+              .then((data) => {
+                let div = document.createElement("div");
+                div.setAttribute("class", "divPresentationSerie")
+                div.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${data.poster_path})`
+                let nomSerie = document.createElement("h2");
+                let genre = document.createElement("p");
+                let production = document.createElement("p");
+                let enCour = document.createElement("p");
+                let description = document.createElement("p");
+                let divFiltre = document.createElement("div")
+                divFiltre.setAttribute("class", "divPresentationFiltre")
+                let saisonsName;
+                let imageSerie;
+                let saisonDescription;
+                let saisonNb;
+  
+                nomSerie.innerHTML = data.name;
+                genre.innerHTML =
+                  "Genres : " + data.genres[0].name + " / " + data.genres[1].name;
+                production.innerHTML =
+                  "Producteur : " +
+                  data.networks[0].name +
+                  " origine : " +
+                  data.networks[0].origin_country +
+                  " nbs episodes : " +
+                  data.number_of_episodes +
+                  " nb saison : " +
+                  data.number_of_seasons;
+                enCour.innerHTML = "Date de l'episode : " + data.last_episode_to_air.air_date + " saison en cour : "
+                  + data.last_episode_to_air.season_number + " épisode en cour : " + data.last_episode_to_air.episode_number
+                description.innerHTML = data.overview
+                
+                  
+                div.appendChild(nomSerie);
+                div.appendChild(enCour);
+                div.appendChild(genre);
+                div.appendChild(production);
+                div.appendChild(description)
+                div.appendChild(divFiltre)
+                grid.appendChild(div);
+  
+  
+                for (let i = 0; i < data.seasons.length; i++) {
+                  divSaison = document.createElement("div")
+                  saisonsName = document.createElement("p");
+                  imageSerie = document.createElement("img");
+                  saisonDescription = document.createElement("p");
+                  saisonNb = document.createElement("p");
+  
+  
+                  saisonsName.innerHTML = data.seasons[i].name;
+                  imageSerie.src =
+                    "https://image.tmdb.org/t/p/original" +
+                    data.seasons[i].poster_path;
+                  saisonDescription.innerHTML = data.seasons[i].overview;
+                  saisonNb.innerHTML =
+                    " Premier episode : " +
+                    data.seasons[i].air_date +
+                    " nbs episodes : " +
+                    data.seasons[i].episode_count;
+  
+  
+                  divSaison.appendChild(saisonsName);
+                  divSaison.appendChild(imageSerie);
+                  divSaison.appendChild(saisonDescription);
+                  divSaison.appendChild(saisonNb);
+                  grid.appendChild(divSaison)
+  
+                } //fin du for
+  
+  
+  
+              });
+          });// fin du bouton
+        } // fin du for
+      }); // fin du fetch
+      //fin cc du fetch
+  
+})
+  //fin de recuperation
   fetch(
     `https://api.themoviedb.org/3/search/tv?api_key=3341d636ea5e718cbe535387f5416379&language=fr&page=1&query=${input}&include_adult=false`
   )
